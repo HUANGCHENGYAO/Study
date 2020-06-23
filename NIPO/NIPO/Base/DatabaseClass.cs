c36537fa4b19e12e22eb9b9c95b0fa4cc83ed427
huangchengyao <huangchengyao@KHPC243>
2020-06-23 11:10:35 +0800

    NIPO_20200623

---
+++ NIPO/NIPO/Base/DatabaseClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace NIPO.Base
+{
+    public class Connection
+    {
+        SqlConnection SqlConection;
+       public Connection()
+        {
+            string ConnnectionStr = System.Configuration.ConfigurationManager.ConnectionStrings["Connection1"].ConnectionString.Replace(" ", "");
+            SqlConection = new SqlConnection(ConnnectionStr);
+        }
+
+        
+        public SqlDataReader db()
+        {
+           
+                SqlDataReader SqlDataReader;
+                SqlConection.Open();
+                SqlCommand SqlCommand = new SqlCommand();
+                SqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                SqlCommand.CommandText = "TEST";
+                SqlCommand.Connection = SqlConection;
+                SqlDataReader = SqlCommand.ExecuteReader();
+                return SqlDataReader;
+                SqlConection.Close();        
+        }
+    }
+}
---
+++ NIPO/NIPO/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using NIPO.Base;
+
+namespace NIPO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.saveFileDialog1.InitialDirectory = "C:\\";    //打开对话框后的初始目录
+            this.saveFileDialog1.Filter = "文本文件|*.txt|所有文件|*.*";
+            this.saveFileDialog1.RestoreDirectory = false;    //若为false，则打开对话框后为上次的目录。若为true，则为初始目录
+            //if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            //{
+            //    MessageBox.Show(System.IO.Path.GetFullPath(saveFileDialog1.FileName));//将选中的文件的路径传递给TextBox “FilePath”);
+            //}
+            Connection conn= new Connection();
+            SqlDataReader db = conn.db();
+            List<string[]> srrinvalue = new List<string[]>();
+            if(db.Read()  ==true)
+                {
+                string[] a = new string[] { };
+                MessageBox.Show(db.FieldCount.ToString());
+                db.GetValues(a);
+                Console.Write(a);
+            }
+
+        }
+    }
+}
