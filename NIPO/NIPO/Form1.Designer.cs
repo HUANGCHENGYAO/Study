c6b2cd9568eb34659a939b5ab4685cf106416c23
huangchengyao <huangchengyao@KHPC243>
2020-06-24 17:44:25 +0800

    20200624

--- NIPO/NIPO/Form1.Designer.cs
+++ NIPO/NIPO/Form1.Designer.cs
@@ -30,26 +30,56 @@
         {
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(72, 43);
+            this.button1.Location = new System.Drawing.Point(320, 48);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "导出";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(96, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "导入";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(96, 140);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.ClientSize = new System.Drawing.Size(858, 407);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -58,6 +88,9 @@
 
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- NIPO/NIPO/Form1.cs
+++ NIPO/NIPO/Form1.cs
@@ -8,7 +8,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
-using NIPO.Base;
 
 namespace NIPO
 {
@@ -21,24 +20,22 @@ namespace NIPO
 
         private void button1_Click(object sender, EventArgs e)
         {
-            this.saveFileDialog1.InitialDirectory = "C:\\";    //打开对话框后的初始目录
-            this.saveFileDialog1.Filter = "文本文件|*.txt|所有文件|*.*";
+            this.saveFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);    //打开对话框后的初始目录
+            this.saveFileDialog1.Filter = "xlsx文件|*.xlsx|所有文件|*.*";
             this.saveFileDialog1.RestoreDirectory = false;    //若为false，则打开对话框后为上次的目录。若为true，则为初始目录
-            //if (saveFileDialog1.ShowDialog() == DialogResult.OK)
-            //{
-            //    MessageBox.Show(System.IO.Path.GetFullPath(saveFileDialog1.FileName));//将选中的文件的路径传递给TextBox “FilePath”);
-            //}
-            Connection conn= new Connection();
-            SqlDataReader db = conn.db();
-            List<string[]> srrinvalue = new List<string[]>();
-            if(db.Read()  ==true)
-                {
-                string[] a = new string[] { };
-                MessageBox.Show(db.FieldCount.ToString());
-                db.GetValues(a);
-                Console.Write(a);
-            }
+            saveFileDialog1.ShowDialog();
+            MessageBox.Show(saveFileDialog1.Filter.ToString());
+          
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.openFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);    //打开对话框后的初始目录
+            this.openFileDialog1.Filter = "xlsx文件|*.txt|所有文件|*.*";
+            this.openFileDialog1.RestoreDirectory = false;    //若为false，则打开对话框后为上次的目录。若为true，则为初始目录
+            openFileDialog1.ShowDialog();
+            MessageBox.Show(openFileDialog1.SafeFileName);
+           
         }
     }
 }
